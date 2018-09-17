61fa5c6967b6ad7265316e288b31535939dbc706
jcryer <jjcryer@mac.com>
2018-09-17 21:21:05 +0100

    First Commit

---
+++ SolarForms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SolarForms.Components;
+
+namespace SolarForms
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            new MainWindow().Run(60);
+        }
+    }
+}
---
+++ SolarForms/bin/Release/Components/Shaders/fragmentShader.c
@@ -0,0 +1,8 @@
+﻿#version 440 core
+
+out vec4 color;
+
+void main(void)
+{
+ color = vec4(1.0, 0.0, 0.0, 1.0);
+}
