443fccbdb6898b1c01adeafa7f4053139d6c054c
unknown <jjcryer@mac.com>
2018-10-03 12:59:59 +0100

    yay

--- SolarForms/Components/MainWindow.cs
+++ SolarForms/Components/MainWindow.cs
@@ -17,7 +17,15 @@ namespace SolarForms.Components
         private List<RenderObject> _renderObjects = new List<RenderObject>();
         private Matrix4 _modelView;
         private Matrix4 _projectionMatrix;
+        private List<SolarObject> _solarObjects = new List<SolarObject>();
 
+        private int mouseX;
+        private int mouseY;
+        private float mouseScroll;
+        private bool isMouseDown;
+        private Vector3 _cameraAngle = new Vector3(0, 0, 0);
+
+        private Vector3 _cameraPosition = new Vector3(0, 0, 0);
         public MainWindow() : base(720, // initial width
         720, // initial height
         GraphicsMode.Default, "SolarCore",  // initial title
@@ -39,8 +47,11 @@ namespace SolarForms.Components
 
         protected override void OnLoad(EventArgs e)
         {
-            VSync = VSyncMode.Off;
+            VSync = VSyncMode.On;
             CreateProjection();
+            _solarObjects.Add(new SolarObject(100, 1, 10, 15, new Vector3(0, 0, -0.1f), new Vector3(6, 6, 12)));
+            _solarObjects.Add(new SolarObject(100, 3, 10, 15, new Vector3(0, 1, -2), new Vector3(4236, 3256, 512)));
+
             _renderObjects.Add(new RenderObject(new Sphere().CreateSphere(3, Color4.White)));
             _renderObjects.Add(new RenderObject(ObjectFactory.CreateSolidCube(0.2f, Color4.BlueViolet)));
             _renderObjects.Add(new RenderObject(ObjectFactory.CreateSolidCube(0.5f, Color4.Red)));
@@ -64,23 +75,72 @@ namespace SolarForms.Components
 
         private void HandleKeyboard()
         {
+            var mouseState = Mouse.GetState();
+            if (mouseScroll != mouseState.Scroll.Y)
+            {
+                _cameraPosition.Z += 0.01f * (mouseState.Scroll.Y - mouseScroll);
+
+                mouseScroll = mouseState.Scroll.Y;
+            }
+
+            if (mouseState.IsButtonDown(MouseButton.Left))
+            {
+                if (isMouseDown == true)
+                {
+                    _cameraPosition.X += 0.001f * (mouseState.X - mouseX);
+                    _cameraPosition.Y += 0.001f * (mouseY - mouseState.Y);
+                }
+                isMouseDown = true;
+                mouseX = mouseState.X;
+                mouseY = mouseState.Y;
+
+            }
+            else
+            {
+                isMouseDown = false;
+            }
             var keyState = Keyboard.GetState();
 
             if (keyState.IsKeyDown(Key.Escape))
             {
                 Exit();
             }
+            if (keyState.IsKeyDown(Key.S))
+            {
+                _cameraPosition.Z -= 0.01f;
+            }
+            if (keyState.IsKeyDown(Key.W))
+            {
+                _cameraPosition.Z += 0.01f;
+            }
+            if (keyState.IsKeyDown(Key.A))
+            {
+                _cameraPosition.X += 0.01f;
+            }
+            if (keyState.IsKeyDown(Key.D))
+            {
+                _cameraPosition.X -= 0.01f;
+            }
+            if (keyState.IsKeyDown(Key.Q))
+            {
+                _cameraPosition.Y += 0.01f;
+            }
+            if (keyState.IsKeyDown(Key.E))
+            {
+                _cameraPosition.Y -= 0.01f;
+            }
         }
 
         private void CreateProjection()
         {
-
             var aspectRatio = (float)Width / Height;
             _projectionMatrix = Matrix4.CreatePerspectiveFieldOfView(
                 60 * ((float)Math.PI / 180f), // field of view angle, in radians
                 aspectRatio,                // current window aspect ratio
                 0.1f,                       // near plane
                 4000f);                     // far plane
+                _projectionMatrix = Matrix4.LookAt(Vector3.UnitZ, Vector3.UnitX
+                    , Vector3.UnitY) * Matrix4.CreatePerspectiveFieldOfView(60 * ((float)Math.PI / 180f),aspectRatio, .01f, 4000.0f);
         }
 
         private void OnClosed(object sender, EventArgs eventArgs)
@@ -107,9 +167,9 @@ namespace SolarForms.Components
             GL.UseProgram(_program);
             GL.UniformMatrix4(20, false, ref _projectionMatrix);
             float c = 0f;
-            foreach (var renderObject in _renderObjects)
+            foreach (var solarObject in _solarObjects)
             {
-                renderObject.Bind();
+                solarObject.Object.Bind();
                 for (int i = 0; i < 1; i++)
                 {
                     var k = i +(float)(_time * (0.05f + (0.1 * c)));
@@ -117,12 +177,14 @@ namespace SolarForms.Components
                            (float)(Math.Sin(k * 5f) * (c + 0.5f)),
                            (float)(Math.Cos(k * 5f) * (c + 0.5f)),
                            -2.7f);*/
-                    var t2 = Matrix4.CreateTranslation(0, 0, -3);
-                    var r1 = Matrix4.CreateRotationZ(-0.5f);
+
+                    var t2 = Matrix4.CreateTranslation(solarObject.Position);
+                    var r1 = Matrix4.CreateRotationZ(solarObject.Obliquity);
                     var r2 = Matrix4.CreateRotationY(0.2f * (float)_time);
-                    var modelView = r2 * t2 * r1;
+                    var lll = Matrix4.CreateScale(0.1f * solarObject.Radius);
+                    var modelView = lll * r2 * t2 * r1 * Matrix4.CreateTranslation(_cameraPosition);// * aaaah;
                     GL.UniformMatrix4(21, false, ref modelView);
-                    renderObject.Render();
+                    solarObject.Object.Render();
                 }
                 c += 0.3f;
             }
---
+++ SolarForms/Components/SolarObject.cs
@@ -0,0 +1,42 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarForms.Components
+{
+    class SolarObject
+    {
+        public int Mass;
+        public int Radius;
+        public int Obliquity;
+        public int OrbitalSpeed;
+
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public RenderObject Object;
+
+        public SolarObject(int mass, int radius, int obliquity, int orbitalSpeed, Vector3 position, Vector3 velocity)
+        {
+            Mass = mass;
+            Radius = radius;
+            Obliquity = obliquity;
+            OrbitalSpeed = orbitalSpeed;
+            Position = position;
+            Velocity = velocity;
+            Object = new RenderObject(new Sphere().CreateSphere(3, Color4.White));
+        }
+
+        public double GetDistance(SolarObject obj)
+        {
+            var x = obj.Position.X - Position.X;
+            var y = obj.Position.Y - Position.Y;
+            var z = obj.Position.Z - Position.Z;
+
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+    }
+}
