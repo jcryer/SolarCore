6750f7af38fb0aa24decd1db7d41b6db6301e944
unknown <jjcryer@mac.com>
2018-11-23 16:01:24 +0000

    le sigh

--- SolarForms/Database/DatabaseMethods.cs
+++ SolarForms/Database/DatabaseMethods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -10,30 +11,21 @@ namespace SolarForms.Database
 {
     class DatabaseMethods
     {
-        public Simulation GetSimulation (int simulationId)
+        public static void GetSimulation (int simulationId)
         {
-            var simQuery = new SQLiteCommand("select * from PlanetarySystem where SimulationID = " + simulationId, Program.DBConnection);
+            string query = "select * from Simulation " +
+                "inner join PlanetarySystem on PlanetarySystem.PlanetarySystemID = Simulation.PlanetarySystemID " +
+                "inner join InitialValues on InitialValues.PlanetarySystemID = Simulation.PlanetarySystemID " +
+                "inner join Object on Object.ObjectID = InitialValues.ObjectID " +
+                "inner join ObjectView on ObjectView.ObjectID = Object.ObjectID " +
+                "where SimulationID = " + simulationId + ";";
+
+            var simQuery = new SQLiteCommand(query, Program.DBConnection);
             var simReader = simQuery.ExecuteReader();
-            simReader.Read();
-            var planetarySystem = new PlanetarySystem((int)simReader[0], (string)simReader[1], (string)simReader[2]);
-
-            var initialValuesQuery = new SQLiteCommand("select * from InitialValues where SimulationID = " + simulationId, Program.DBConnection);
-            var initialValuesReader = initialValuesQuery.ExecuteReader();
-
-            while (initialValuesReader.Read())
+            while (simReader.Read())
             {
-                var initialValue = new ObjectInitialValue((int)initialValuesReader[0], (int)initialValuesReader[1], (float)initialValuesReader[2], (float)initialValuesReader[3], (float)initialValuesReader[4], (float)initialValuesReader[2], (float)initialValuesReader[5], (float)initialValuesReader[6]);
-
-                var objectQuery = new SQLiteCommand("select * from Object where ObjectID = " + initialValue.ObjectID, Program.DBConnection);
-                var objectReader = objectQuery.ExecuteReader();
-                var o = new Object((int)simReader[0], (string)simReader[1], (double)simReader[2], (double)simReader[3], (double)simReader[4], (double)simReader[5]);
-
+                continue;
             }
-            var c = new SQLiteCommand("select * from Object where ObjectID = " + id, Program.DBConnection);
-            var reader = c.ExecuteReader();
-            reader.Read();
-            new SolarObject((string)reader[1], (double)reader[2], (double)reader[3], (double)reader[4], (double)reader[5]);
-                //(new Object((int)reader[0], (string)reader[1], (double)reader[2], (double)reader[3], (double)reader[4], (double)reader[5]));
             
         }
 
--- SolarForms/Program.cs
+++ SolarForms/Program.cs
@@ -16,8 +16,62 @@ namespace SolarForms
 
             DBConnection = new SQLiteConnection("Data Source=SolarDB.sqlite;Version=3;");
             DBConnection.Open();
-            SQLiteCommand c = new SQLiteCommand("create table if not exists ObjectInitialValues(ObjectID int, PlanetarySystemID int,  PositionX real, PositionY real, PositionZ real, VelocityX real, VelocityY real, VelocityZ real)", DBConnection);
-            c.ExecuteNonQuery();
+
+            SQLiteCommand Object = new SQLiteCommand("" +
+                "CREATE TABLE IF NOT EXISTS `Object` (" +
+                "`ObjectID`	INTEGER PRIMARY KEY AUTOINCREMENT," +
+                 "`Name`	TEXT NOT NULL," +
+                "`Mass`	REAL NOT NULL," +
+                "`Radius`	REAL NOT NULL," +
+                "`Obliquity`	REAL NOT NULL," +
+                "`OrbitalSpeed`	REAL NOT NULL); ", DBConnection);
+
+            SQLiteCommand InitialValues = new SQLiteCommand("" +
+                "CREATE TABLE IF NOT EXISTS `InitialValues` (" +
+                "`ObjectID`	INTEGER NOT NULL," +
+                "`PlanetarySystemID`	INTEGER NOT NULL," +
+                "`PositionX`	REAL NOT NULL," +
+                "`PositionY`	REAL NOT NULL," +
+                "`PositionZ`	REAL NOT NULL," +
+                "`VelocityX`	REAL NOT NULL," +
+                "`VelocityY`	REAL NOT NULL," +
+                "`VelocityZ`	REAL NOT NULL," +
+                "foreign key(ObjectID) references Object(ObjectID)," +
+                "foreign key(PlanetarySystemID) references PlanetarySystem(PlanetarySystemID));", DBConnection);
+
+            SQLiteCommand PlanetarySystem = new SQLiteCommand("" +
+                "CREATE TABLE IF NOT EXISTS `PlanetarySystem` (" +
+                "`PlanetarySystemID`	INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "`Name`	TEXT NOT NULL," +
+                "`Description`	TEXT NOT NULL); ", DBConnection);
+
+            SQLiteCommand Simulation = new SQLiteCommand("" +
+                "CREATE TABLE IF NOT EXISTS `Simulation` (" +
+                "`SimulationID`	INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "`PlanetarySystemID`	INTEGER NOT NULL," +
+                "`Speed`	REAL NOT NULL," +
+                "`Zoom`	REAL NOT NULL," +
+                "`InitialFocus`	INTEGER NOT NULL," +
+                "foreign key(PlanetarySystemID) references PlanetarySystem(PlanetarySystemID)); ", DBConnection);
+
+            SQLiteCommand ObjectView = new SQLiteCommand("" +
+            "CREATE TABLE IF NOT EXISTS `ObjectView` (" +
+            "`PerformanceID`	INTEGER NOT NULL," +
+            "`ObjectID`	INTEGER NOT NULL," +
+            "`TrailActive`	INTEGER NOT NULL," +
+            "`TrailLength`	INTEGER NOT NULL," +
+            "`TrailColour`	INTEGER NOT NULL," +
+            "`ObjectColour`	INTEGER NOT NULL," +
+            "foreign key(PerformanceID) references Performance(PerformanceID)," +
+            "foreign key(ObjectID) references Object(ObjectID)); ", DBConnection);
+
+            Object.ExecuteNonQuery();
+            InitialValues.ExecuteNonQuery();
+            PlanetarySystem.ExecuteNonQuery();
+            Simulation.ExecuteNonQuery();
+            ObjectView.ExecuteNonQuery();
+
+            Database.DatabaseMethods.GetSimulation(0);
             //   var obj = JsonConvert.DeserializeObject<SolarObject>(File.ReadAllText("PlanetaryData/Earth.json"));
 
             //    new Components.Menus.ObjectForm(obj).Show();
